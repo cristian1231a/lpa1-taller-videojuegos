73b8143daab2b344c2e9401799456888709c9697
DanielAngul0 <daniel.andres.angulo.perez>
2025-04-26 22:39:31 -0500

    Organizando directorios

--- entidades/jugador.py
+++ entidades/jugador.py
@@ -136,7 +136,7 @@ class Jugador(Personaje):
         self.experiencia = 0
         self.nivel_xp = NivelXP()
         self.inventario = []  # Lista de inventario
-        self.dinero = 5000
+        self.dinero = 500
         self.capas_defensa = 0
         self.puntos_vida_max = puntos_vida_inicial
 
